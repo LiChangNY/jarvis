4cddeec23135fde111110ee6206792ef0611bfb3
Lily Chang <lchang@indeed.com>
2016-07-03 20:20:14 -0400

    Replaced hardcoded testing df with paramter

--- twoAxesLineChart.py
+++ twoAxesLineChart.py
@@ -1,39 +1,34 @@
 from IPython.display import display, HTML
 import jinja2
 import os
+import pandas as pd
 
 jinja_environment = jinja2.Environment(autoescape=True,
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
-class twoAxesLineChart():
+class twoAxesLineChart(object):
 
     def __init__(self):
         pass
 
-    def draw(self, **kwarg):
+    def draw(self, dataframe, **kwarg):
 
-      templates_var = {'data': """[
-      {'Date': '2015-01-01', "Series 1":100 , "Series 2": 200 },
-      {'Date': '2015-01-15', "Series 1":20 , "Series 2": 60 },
-      {'Date': '2015-02-01', "Series 1":200 , "Series 2": 200 },
-      {'Date': '2015-02-15', "Series 1":40 , "Series 2": 90 },
-      {'Date': '2015-03-01', "Series 1":300 , "Series 2": 400 },
-     ]"""};
+        df_json = dataframe.to_json(orient='records')
+        templates_var = {'data': df_json}
 
+        pyd3_js_url = "files/d3.pyd3.js"
+        d3_js_url ="files/d3.v3.min.js"
+        css_url = "files/d3.pyd3.css"
 
-      pyd3_js_url = "files/d3.pyd3.js"
-      d3_js_url ="files/d3.v3.min.js"
-      css_url = "files/d3.pyd3.css"
-
-      display(HTML('''<link media="all" href="%s" type="text/css"
+        display(HTML('''<link media="all" href="%s" type="text/css"
                       rel="stylesheet"/>''' % (css_url)))
-      display(HTML('<script src="%s"></script>' % (d3_js_url)))
-      display(HTML('<script src="%s"></script>' % (pyd3_js_url)))
-      display(HTML('<script src="%s"></script>' % (d3_js_url)))
+        display(HTML('<script src="%s"></script>' % (d3_js_url)))
+        display(HTML('<script src="%s"></script>' % (pyd3_js_url)))
+        display(HTML('<script src="%s"></script>' % (d3_js_url)))
 
-      TEMPLATE_FILE = "twoAxesLineChart.html"
-      template = jinja_environment.get_template( TEMPLATE_FILE )
+        TEMPLATE_FILE = "twoAxesLineChart.html"
+        template = jinja_environment.get_template( TEMPLATE_FILE )
 
-      html = template.render(templates_var)
+        html = template.render(templates_var)
 
-      display(HTML(html))
+        display(HTML(html))
