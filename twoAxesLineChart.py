7e91f80344100cfbf67367356e4454fab141210d
Lily Chang <lchang@indeed.com>
2016-07-28 15:29:16 -0400

    Added bootstrap multiselect dropdown

--- twoAxesLineChart.py
+++ twoAxesLineChart.py
@@ -20,6 +20,15 @@ class twoAxesLineChart(object):
     display(HTML("<script src='%s'></script>" % (d3_js_url)))
     display(HTML("<script src='%s'></script>" % (pyd3_js_url)))
 
+
+    display(HTML("""<link rel="stylesheet" href="files/bootstrap.min.css" type="text/css"/>"""))
+    display(HTML("""<script type="text/javascript" src="files/jquery.min.js"></script>"""))
+    display(HTML("""<script type="text/javascript" src="files/bootstrap.min.js"></script>"""))
+
+    display(HTML("""<script type="text/javascript" src="files/bootstrap-multiselect.js"></script>"""))
+    display(HTML("""<link rel="stylesheet" href="files/bootstrap-multiselect.css" type="text/css"/>"""))
+
+
     def __init__(self, dataframe, **kwargs):
         self.TEMPLATE_FILE = "twoAxesLineChart.html"
         self.df_json = dataframe.to_json(orient='records')
@@ -30,7 +39,18 @@ class twoAxesLineChart(object):
         self.x_serie = kwargs.get("x_serie", dataframe_columns[0])
         self.y_series = kwargs.get("y_series", dataframe_columns[1:self.series_count])
 
-        # Accepted kwargs for canvas element
+        self.filter_series = kwargs.get("filter_series", None)
+        self.filter_values = {}
+        if self.filter_series is not None:
+
+            if not isinstance(self.filter_series, list):
+                self.filter_series = [self.filter_series]
+
+            for f in self.filter_series:
+               self.filter_values[f] = list(dataframe[f].unique())
+
+
+        # Accepted kwargs for DOM element
         self.x_axis_title = kwargs.get("x_axis_title", "")
         self.y_axis_title = kwargs.get("y_axis_title", "")
         self.second_y_axis_title = kwargs.get("second_y_axis_title", "")
@@ -43,17 +63,21 @@ class twoAxesLineChart(object):
         self.canvas_height = kwargs.get("canvas_height", 400)
         self.width = kwargs.get('width', self.canvas_width - self.margin_left - self.margin_right)
         self.height = kwargs.get('height', self.canvas_height - self.margin_top - self.margin_bottom)
-        self.legendTick = kwargs.get("legendTick", 'bar')
-
-        # Accepted kwargs for shape element
+        self.legend_tick = kwargs.get("legend_tick", 'bar')
+        self.legend_style = kwargs.get("legend_style", "expand")
         self.interpolation_method = kwargs.get("interpolation_method",  'cardinal')
+
+
+
+
 
     def show(self):
 
         template = jinja_environment.get_template(self.TEMPLATE_FILE)
 
         html_content = template.render(chart={"options": json.dumps(self, default=lambda o: o.__dict__),
-                                              "data": self.df_json})
+                                              "data": self.df_json,
+                                              "filters": self.filter_values})
 
         display(HTML(html_content))
 
