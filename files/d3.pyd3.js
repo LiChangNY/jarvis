eb97a8f9088daab0abecfb463efcbc33f092d091
Lily Chang <lchang@indeed.com>
2016-08-14 21:50:26 -0400

    Use LineChartBuilder to encapsulate common methods.

--- files/d3.pyd3.js
+++ files/d3.pyd3.js
@@ -1,18 +1,31 @@
 var pyd3 = pyd3 || {};
 
-pyd3.drawChart = (function() {
+pyd3.LineChart = (function() {
 
-LineChart = function(id, width, height, position, colorSet = d3.scale.category10()) {
+LineChartBuilder = function(id, width, height, margin, x_serie, y_series, xScale, yScale, colorSet = d3.scale.category10()) {
+    var seriesCount = y_series.length;
+    var formatDate = d3.time.format("%Y-%m-%d");
 
-    var drawCanvas = function(id, width, height, position) {
+    var drawCanvas = function() {
         return d3
             .select(id)
-                .append("svg")
+            .append("svg")
                 .style("max-width", "960px")
                 .attr("width", width)
                 .attr("height", height)
             .append("g")
-                .attr("transform", "translate(" + position.x + "," + position.y + ")");
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    }
+
+    this.drawGridLine = function() {
+        // Draw the left y Grid lines
+        svg.append("g")
+          .attr("class", "grid")
+          .call(axisBuilder.make(yScale, "left", 5)
+                  .tickSize(-width, 0, 0)
+                  .tickFormat("")
+               )
+         return this;
     }
 
     this.drawTitle = function(x, y, title) {
@@ -58,19 +71,12 @@ LineChart = function(id, width, height, position, colorSet = d3.scale.category10
         return this;
     }
 
-    this.setScale = function(xScale, yScale) {
-        this.xScale = xScale;
-        this.yScale = yScale;
-
-        return this;
-    }
-
 
     this.drawLine = function(dataGrouped) {
         var line = d3.svg.line()
             .interpolate('cardinal')
-            .x(xScale)
-            .y(yScale);
+            .x(function(d) { return xScale(d[x_serie]); })
+            .y(function(d) { return yScale(d[d.id]); });
 
         //Update svg
         var multiLines = svg
@@ -111,7 +117,7 @@ LineChart = function(id, width, height, position, colorSet = d3.scale.category10
 
             if (!value) return true;
 
-            if (value instanceof Date) value=d3.time.format("%Y-%m-%d")(value);
+            if (value instanceof Date) value=formatDate(value);
 
             return valueSelected.indexOf(value.toString()) > -1;
         };
@@ -135,74 +141,154 @@ LineChart = function(id, width, height, position, colorSet = d3.scale.category10
         return this;
     };
 
-    this.svg = drawCanvas(id, width, height, position);
+    this.drawLegend = function(height, position, legendTick, circleRadius) {
+        this.legendHeight = height;
+
+        this.legend = svg.append("g")
+            .attr("class","legend")
+            .attr("transform", "translate(" + position.x + "," + position.y + ")")
+            .selectAll("g")
+            .data(d3.range(seriesCount))
+            .enter().append("g");
+
+        if (legendTick == 'circle') {
+            drawCircleTicks();
+        } else {
+            drawRectTicks();
+        };
+
+        colorTicks();
+        drawLabel();
+
+        return this;
+    }
+
+    var drawCircleTicks = function() {
+        return legend.append("circle")
+                .attr("class", "legend tick")
+                .attr("cx", function(d) { return margin.right / 4;})
+                .attr("cy", function(d) { return d*(legendHeight/seriesCount); })
+                .attr("r", function (d) { return circleRadius; })
+
+    }
+
+    var drawRectTicks = function(){
+
+        return legend.append("rect")
+                .attr("class", "legend tick")
+                .attr('x', function(d) { return margin.right/5; })
+                .attr('y', function(d) { return Math.floor(d*legendHeight/seriesCount); })
+                .attr("width", 10)
+                .attr("height", Math.floor(legendHeight/seriesCount) )
+
+    }
+
+    var colorTicks = function() {
+        return legend.selectAll('.legend.tick')
+               .attr("id", function(d) {return "legend-tick-" + d; })
+               .style("fill", function(d) {return colorSet(d);})
+               .style('stroke', function(d) {return colorSet(d);})
+               .style('stroke-opacity',0.3)
+                .on("click", function(d) {
+                    toggleId('line-'+d);
+                    toggleId('focus-'+d)
+                    toggleId('legend-tick-'+d)
+                })
+    }
+
+
+    var drawLabel = function(){
+
+        return legend.append('text')
+                .attr("y", function (d) { return (d+0.5)*(legendHeight/seriesCount); })
+                .attr("x", function (d) { return  margin.right/3;})
+                .attr("fill", function (d) {return colorSet(d); })
+                .attr("text-anchor", "start")
+                .on("click", function(d) {
+                    toggleId('line-'+d);
+                    toggleId('focus-'+d)
+                    toggleId('legend-tick-'+d)
+                })
+                .text(function(d) {return y_series[d]})
+
+    }
+
+    var toggleId = function(shape_id) {
+        //Need to add [0] to access the attributes
+        var active  = $("#"+shape_id)[0].active ? false : true
+		, newOpacity = active ? 0 : 1;
+
+		// Hide or show the elements
+		d3.select("#"+shape_id).style("opacity", newOpacity);
+
+		// Update whether or not the elements are active
+		$("#"+shape_id)[0].active = active;
+		//console.log($("#"+shape_id)[0].active);
+
+    }
+
+
+    //Draw focus circle
+    var drawFocusCircle = function() {
+        var focus = svg.selectAll(".focus")
+            .data(d3.range(seriesCount))
+            .enter()
+            .append("g")
+            .attr("class", "focus")
+            .style('stroke', function(d) { return colorSet(d % 10); })
+            .style("display", "none")
+            .attr('id', function(d) { return 'focus-'+d; });
+
+        focus.append("circle")
+            .attr("r", 4.5);
+        focus.append("text")
+            .attr("x", 9)
+            .attr("dy", ".35em");
+    };
+
+    var changeFocusCircleState = function(state) {
+        svg.selectAll(".focus")
+           .style("display", state);
+    };
+
+
+    var mouseMoveFocusCircle = function(data, element){
+        var bisectDate = d3.bisector(function(d) { return d[x_serie]; }).left;
+
+        var x0 = xScale.invert(d3.mouse(element)[0]),
+            i = bisectDate(data, x0, 1),
+            d0 = data[i - 1],
+            d1 = data[i],
+            d = x0 - d0[x_serie] > d1[x_serie] - x0 ? d1 : d0;
+
+        svg.selectAll(".focus")
+            .attr("transform", function(i) { return "translate(" + xScale(d[x_serie])  + ","
+                   + yScale(d['Series ' + (i + 1)]) + ")";})
+            .select("text")
+                .text(function(i) { return formatDate(d[x_serie]) + ": "+ d['Series ' + (i + 1)]; })
+                .style('fill', function(i) { return colorSet(i % 10); })
+                .style('font-size', '10px');
+    };
+
+
+    this.updateFocusCircle = function(data, width, height) {
+        drawFocusCircle();
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .on("mouseover", function() {changeFocusCircleState(null);})
+            .on("mouseout", function() {changeFocusCircleState('none');})
+            .on("mousemove", function() {mouseMoveFocusCircle(data, this)})
+
+        return this;
+    };
+
+    this.svg = drawCanvas();
 
     return this;
 }
 
-
-var legendBuilder = {};
-
-legendBuilder.drawLegend = function(svg, position, value) {
-    var legend = svg.append("g")
-        .attr("class","legend")
-        .attr("transform", "translate(" + position.x + "," + position.y + ")")
-        .selectAll("g")
-        .data(value)
-        .enter().append("g");
-
-    return legend;
-}
-
-legendBuilder.drawCircleTicks = function(legend, cx, cy, r){
-    return legend.append("circle")
-            .attr("class", "legend tick")
-            .attr("cx", function(d) {return cx;})
-            .attr("cy", function(d) {return cy; })
-            .attr("r", function (d) { return r; })
-
-}
-
-legendBuilder.drawRectTicks = function(legend, x, y, w, h){
-
-    return legend.append("rect")
-            .attr("class", "legend tick")
-            .attr('x', function(d) { return x; })
-            .attr('y', function(d) {return y; })
-            .attr("width", w)
-            .attr("height", h )
-
-}
-
-legendBuilder.colorTicks = function(legend, colorSet) {
-    return legend.selectAll('.legend.tick')
-           .attr("id", function(d) {return "legend-tick-" + d; })
-           .style("fill", function(d) {return colorSet(d % colorSet.length);})
-           .style('stroke', function(d) {return colorSet(d % colorSet.length);})
-           .style('stroke-opacity',0.3)
-            .on("click", function(d) {
-                toggleId('line-'+d);
-                toggleId('focus-'+d)
-                toggleId('legend-tick-'+d)
-            })
-}
-
-
-legendBuilder.drawLabel = function(legend, x, y, colorSet, labels){
-
-    return legend.append('text')
-            .attr("y", function (d) { return y; })
-            .attr("x", function (d) { return  x;})
-            .attr("fill", function (d) {return c10(d); })
-            .attr("text-anchor", "start")
-            .on("click", function(d) {
-                toggleId('line-'+d);
-                toggleId('focus-'+d)
-                toggleId('legend-tick-'+d)
-            })
-            .text(function(d) {return labels[d]})
-
-}
 
 
 var scaleBuilder = {}
@@ -236,24 +322,6 @@ axisBuilder.make = function(scale, orientation, ticks) {
 
 var drawChart = function(data, options, filters) {
 
-    filterDict = {};
-
-    for (var f in filters) {
-
-        var f_slugify = f.replace(' ', '-').toLowerCase();
-
-        $("#filter-"+f_slugify).multiselect({
-            enableCaseInsensitiveFiltering: true,
-            includeSelectAllOption: true,
-            //Bootstrap thing. Have to define update functions for all three events.
-            onChange: function(option, checked) { chart.updateData(this.$select, filterDict, nestData, data, xAxis);},
-            onSelectAll: function(checked) { chart.updateData(this.$select, filterDict, nestData, data, xAxis); },
-            onDeselectAll: function(checked) { cart.updateData(this.$select, filterDict, nestData, data, xAxis); }
-        })
-
-    }
-
-
     //*** Init attributes *** //
     var x_serie =  options.x_serie
     , y_series = options.y_series
@@ -281,16 +349,14 @@ var drawChart = function(data, options, filters) {
     , circleRadius= options.circleRadius || 5
 
 
-    var parseDate = d3.time.format('%Y-%m-%d').parse,
-      bisectDate = d3.bisector(function(d) { return d[x_serie]; }).left,
-      formatDate = d3.time.format("%Y-%m-%d");
+    var parseDate = d3.time.format('%Y-%m-%d').parse;
 
     data.forEach(function(d) {
 
-     d[x_serie] = parseDate(d[x_serie])
-     y_series.forEach(function(item, index){
-        d[item] = +d[item];
-      })
+         d[x_serie] = parseDate(d[x_serie])
+         y_series.forEach(function(item, index){
+            d[item] = +d[item];
+          })
 
     });
 
@@ -299,13 +365,30 @@ var drawChart = function(data, options, filters) {
       return a.x_serie - b.x_serie;
     })
 
+    filterDict = {};
+
+    for (var f in filters) {
+
+        var f_slugify = f.replace(' ', '-').toLowerCase();
+
+        $("#filter-"+f_slugify).multiselect({
+            enableCaseInsensitiveFiltering: true,
+            includeSelectAllOption: true,
+            //Bootstrap thing. Have to define update functions for all three events.
+            onChange: function(option, checked) { chart.updateData(this.$select, filterDict, nestData, data, xAxis);},
+            onSelectAll: function(checked) { chart.updateData(this.$select, filterDict, nestData, data, xAxis); },
+            onDeselectAll: function(checked) { cart.updateData(this.$select, filterDict, nestData, data, xAxis); }
+        })
+
+    }
+
 
     //define axis
     var xScale = scaleBuilder.timeScale([0, width - margin.right / 2], d3.extent(data, function(d) { return d.Date; }));
 
     var xAxis = axisBuilder.make(xScale, "bottom");
 
-    nestData = function(data) {
+    var nestData = function(data) {
         return y_series.map(function(e) {
             return data.map(function(d) {
                 var value = {};
@@ -330,136 +413,15 @@ var drawChart = function(data, options, filters) {
 
     var yAxis = axisBuilder.make(yScale, "left", data.length);
 
-    var chart = LineChart('#chart1', canvas_width, canvas_height, {x: margin.left , y: margin.top} )
+    var chart = LineChartBuilder('#chart1', canvas_width, canvas_height, margin, x_serie, y_series, xScale, yScale)
         .drawTitle(width / 2, margin.top / 2, chart_title)
         .drawXAxis(xAxis, axisPosition={x: 0, y: height},
                     titlePosition={x: width/2, y: margin.bottom/2}, x_axis_title)
         .drawYAxis(yAxis, {x:-height/2, y:-margin.left/2}, y_axis_title)
-        .setScale(function(d) { return xScale(d[x_serie]); }, function(d) { return yScale(d[d.id]); })
-        .drawLine(dataGrouped);
-        //.drawLegend();
-
-    // Draw the left y Grid lines
-    chart.svg.append("g")
-      .attr("class", "grid")
-      .call(axisBuilder.make(yScale, "left", 5)
-              .tickSize(-width, 0, 0)
-              .tickFormat("")
-           )
-
-    //*** Set up shapes *** //
-    var c10 = d3.scale.category10();
-
-
-    //Draw focus circle
-    drawFocusCircle = function(serieName, serieNo) {
-        var focus = chart.svg.append("g")
-            .attr("class", "focus")
-            .style('stroke', c10(serieNo % 10))
-            .style("display", "none")
-            .attr('id', 'focus-'+serieNo)
-
-         focus.append("circle")
-            .attr("r", 4.5)
-
-         focus.append("text")
-            .attr("x", 9)
-            .attr("dy", ".35em");
-
-        window['focus-' + serieNo] = focus;
-
-    };
-
-    changeFocusCircleState = function(state) {
-       y_series.forEach( function(serieName, serieNo) {
-            window['focus-'+serieNo].style("display", state);
-       })
-    };
-
-
-    mouseMoveFocusCircle = function(data, element) {
-
-        var x0 = xScale.invert(d3.mouse(element)[0]),
-            i = bisectDate(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0[x_serie] > d1[x_serie] - x0 ? d1 : d0;
-
-
-        y_series.forEach( function(serieName, serieNo){
-            window['focus-'+serieNo].attr("transform", "translate(" + xScale(d[x_serie])  + ","
-                   + yScale(d[serieName]) + ")");
-            window['focus-'+serieNo].select("text")
-                .text(formatDate(d[x_serie]) + ": "+ d[serieName])
-                .style('fill', c10(serieNo % 10))
-                .style('font-size', '10px');
-
-        });
-
-    };
-
-
-    updateFocusCircle = function(data) {
-
-        y_series.forEach(function(serieName, serieNo) {
-
-            drawFocusCircle(serieName,serieNo);
-
-        });
-
-        if (data && data.length > 0) {
-          chart.svg.append("rect")
-            .attr("class", "overlay")
-            .attr("width", width)
-            .attr("height", height)
-            //Why can't we just call changeFocusCircleState(state) but function()
-            .on("mouseover", function() {changeFocusCircleState(null);})
-            .on("mouseout", function() {changeFocusCircleState('none');})
-            .on("mousemove", function() {mouseMoveFocusCircle(data, this)})
-
-        } else {
-
-            y_series.forEach(function(serieName, serieNo) {
-
-                window['focus-'+serieNo].remove();
-            });
-
-        }
-
-
-    };
-
-    //updateFocusCircle(data);
-
-    legend = legendBuilder.drawLegend(chart.svg, {x: legend_x, y: legend_y}, d3.range(y_series.length))
-    if (legendTick == 'circle') {
-        legendBuilder.drawCircleTicks(legend,
-                                       margin.right/4 ,
-                                       d*(height/y_series.length),
-                                       circleRadius)
-    } else {
-        legendBuilder.drawRectTicks(legend, margin.right/5, Math.floor(d*(height)/y_series.length),
-                                10, Math.floor(height/y_series.length))
-    };
-
-    legendBuilder.colorTicks(legend, c10);
-    legendBuilder.drawLabel(legend, margin.right/3, (d+0.5)*(height/y_series.length), c10, y_series)
-
-    toggleId = function(shape_id) {
-        //Need to add [0] to access the attributes
-        var active  = $("#"+shape_id)[0].active ? false : true
-		, newOpacity = active ? 0 : 1;
-
-		// Hide or show the elements
-		d3.select("#"+shape_id).style("opacity", newOpacity);
-
-		// Update whether or not the elements are active
-		$("#"+shape_id)[0].active = active;
-		//console.log($("#"+shape_id)[0].active);
-
-    }
-
-
+        .drawLine(dataGrouped)
+        .drawLegend(height, {x: legend_x, y: legend_y}, legendTick, circleRadius)
+        .drawGridLine()
+        .updateFocusCircle(data, width, height);
 }
 
 return drawChart;
